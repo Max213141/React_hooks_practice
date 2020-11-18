e826f652d7db530f0b5dea50498c38c6f007daf2
Max213141 <66839835+Max213141@users.noreply.github.com>
2020-11-18 18:06:39 +0300

    added alert popup, abiility to interract with github API

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {AlertContext} from "../context/alert/alertContext";
 
-export const Alert= (alert) => {
+
+export const Alert= ({}) => {
+    const {alert, hide} = useContext(AlertContext)
+
+    if (!alert) return null
 
     return(
-            <div className={`alert alert-${alert.type || 'secondary'} alert-dismissible`}
-                 role="alert"
+        <div
+            className={`alert alert-${alert.type || 'secondary'} alert-dismissible fade show`}
+             role="alert"
+        >
+            {alert.text}
+            <button type="button" className="close" aria-label="Close"
+                    onClick={hide}
             >
-                {alert.text}
-                <button type="button"
-                        className="close"
-                        aria-label="Close"
-                >
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
     )
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, {useContext, useState} from 'react'
+import {AlertContext} from "../context/alert/alertContext";
+import {GithubContext} from "../context/github/githubContext";
 
 export const Search = () =>{
+    const [value, setValue] = useState('')
+    const {show}=useContext(AlertContext)
+    const github = useContext(GithubContext)
+
+    const onSubmit = event => {
+        if (event.key !== 'Enter'){
+            return
+        }
+
+        if (value.trim()) {
+            github.search(value.trim())
+        } else {
+            show('Enter info')
+        }
+    }
+
     return (
         <div className="form-group">
             <input type="text"
                    className="form-control"
                    placeholder="Enter username..."
+                   vlaue={value}
+                   onChange={event => setValue(event.target.value)}
+                   onKeyPress={onSubmit}
             />
         </div>
     )
